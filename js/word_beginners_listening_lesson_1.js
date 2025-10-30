b517d1b382d103b84a2304455857544c2c6f1647
JKC-JP-class <jkcjplanguageclass@gmail.com>
2025-10-30 18:34:44 +0900

    Add files via upload

--- js/word_beginners_listening_lesson_1.js
+++ js/word_beginners_listening_lesson_1.js
@@ -1,7 +1,9 @@
 const datas = [
+
     { file: "listening_data/Konnichiwa.m4a", answer: "konnichiwa" },
     { file: "listening_data/Konbanwa.m4a", answer: "konbanwa" },
     { file: "listening_data/Sayounara.m4a", answer: "sayounara" },
     { file: "listening_data/Arigatou.m4a", answer: "arigatou" },
-    { file: "listening_data/Mataashita.m4a", answer: "mataashita" }
+    { file: "listening_data/Mataashita.m4a", answer: "mataashita" },
+    
   ];
