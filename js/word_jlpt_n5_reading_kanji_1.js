a83d08c86ed4b7e7fec8ce4529942da6e524253f
JKC-JP-class <jkcjplanguageclass@gmail.com>
2025-12-18 18:24:00 +0900

    Add files via upload
    251218-01　kanji-trainer added for trial

---
+++ js/word_jlpt_n5_reading_kanji_1.js
@@ -0,0 +1,8 @@
+const words = [
+    { question: "日", answer: ["jitsu","nichi","hi","bi","ka" ]},
+    { question: "一", answer: ["ichi","hito" ]},
+    { question: "国", answer: ["kuni","koku" ]},
+    { question: "人", answer: ["jin","ninn" ]},
+    { question: "年", answer: ["nen","toshi" ]},
+    { question: "大", answer: ["dai","tai","oo" ]},
+];
